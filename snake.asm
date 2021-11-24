647e87f3e2f1d9cbb8d2331df2ecd53275e2f6b3
Selim Jerad <selim.jerad@epfl.ch>
2021-11-24 12:02:42 +0100

    Update snake.asm

--- snake.asm
+++ snake.asm
@@ -67,6 +67,7 @@ main:
 
         ; BEGIN: loop_get_input
         loop_get_input:
+            call wait_procedure
             call get_input
             add t0, zero, v0 ; load which button pressed
             addi t1, zero, 5
