bbd3feacf814ca9fe94cf7b37ea1f0a0cdf959e2
marc-antoineallard74 <allard.marcantoine@outlook.fr>
2021-11-24 11:11:15 +0100

    final

--- snake.asm
+++ snake.asm
@@ -43,6 +43,9 @@
 .equ    ARG_HUNGRY,     0       ; a0 argument for move_snake when food wasn't eaten
 .equ    ARG_FED,        1       ; a0 argument for move_snake when food was eaten
 
+;Param
+.equ    TIMER,          5000   ; latence 
+
 
 ; initialize stack pointer
 addi    sp, zero, LEDS
@@ -213,7 +216,32 @@ set_pixel:
 
 ; BEGIN: display_score
 display_score:
+    ldw t0, SCORE(zero) ;load the score 
 
+    addi t1, zero, zero
+    addi t2, zero, zero
+
+    ;BEGIN: unit_loop
+    unit_loop:
+        addi t2, t2, 1
+        addi t0, t0, -10
+        blt t0, zero, set_process
+        br unit_loop
+    ; END: unit_loop
+
+    ; BEGIN: set_process
+    set_process:
+        addi t1, t1, 10
+        addi t2, t2, -1
+
+        ret
+    ;END: set_process
+
+    ldw t1, digit_map(t1) ; load
+    ldw t2, digit_map(t2) ; load
+    stw t1, SEVEN_SEGS+12(zero) ; change the value 
+    stw t2, SEVEN_SEGS+8(zero) ; change the value 
+    
 ; END: display_score
 
 
@@ -880,5 +908,56 @@ copy_memory:
 
 ; BEGIN: blink_score
 blink_score:
+    addi s1, zero, 6
+
+    ; BEGIN: blink_procedure
+    blink_procedure:
+        slli t0, s1, 1              ; modulo 2 
+        beq t0, zero, display_score ; if the counter modulo 2 then switch on the light
+        bne t0, zero, switch_off    ; if the counter is not modulo 2 then switch off 
+        call wait_procedure         ; call the waiting procedure 
+        addi s1, s1, -1
+        jmpi blink_procedure
+    ; END: blink_procedure
+
+    ; BEGIN: switch_off
+    switch_off:
+        stw zero, SEVEN_SEGS(zero)   ; switch off the light
+        stw zero, SEVEN_SEGS+4(zero) ; switch off the light
+        stw zero, SEVEN_SEGS+8(zero) ; switch off the light 
+        stw zero, SEVEN_SEGS+12(zero); switch off the light 
+    ; END: switch_off 
 
 ; END: blink_score
+
+; BEGIN: wait_procedure
+wait_procedure:
+
+    addi s0, zero, TIMER
+    slli s0, s0, 10 
+
+    ; BEGIN
+    loop_time:
+        beq s0, zero, return_procedure
+        addi s0, t0, -1
+    ; END
+
+    ; BEGIN
+    return_procedure:
+        ret
+    ; END
+
+; END: wait_procedure
+
+; BEGIN: digit_map
+digit_map:
+    .word 0xFC ; 0
+    .word 0x60 ; 1
+    .word 0xDA ; 2
+    .word 0xF2 ; 3
+    .word 0x66 ; 4
+    .word 0xB6 ; 5
+    .word 0xBE ; 6
+    .word 0xE0 ; 7
+    .word 0xFE ; 8
+    .word 0xF6 ; 9
